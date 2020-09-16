888251c52fbe0e80e162852258ead77712660f4c
rainecortel <lorraine_renee_cortel@dlsu.edu.ph>
2020-09-17 04:47:32 +0800

    09-17

---
+++ heart_failure.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+
+class HeartFailure(object):
+
+    def __init__(self):
+        """ Class constructor for HeartFailure.
+        """
+
+    def check_if_int(self, data):
+        """ This function determines whether a column Series contains
+        values that are all integers or not. Depending on the answer,
+        the return type could change.
+        
+        Arguments:
+            data {Series} - a column Series
+        Returns:
+            There are two options that can be returned:
+                1. Boolean - True or False (If data is all integers).
+                2. Boolean and Vector Array (If data contains at least 
+                    one non-integer value).
+        """
+        # Create a temporary blank list.
+        temp = []
+        
+        # Iterate over the elements in data and check whether they are an 
+        # integer using the modulo logic. If the data is not an integer,
+        # add the index of that element to the list called temp.
+        for x in range(len(data)):
+            if data[x] % 1 != 0:
+                temp.append(x)
+        
+        # Assume that all values are integers.
+        is_int = True
+        
+        # Check whether the count of temp list exceeds 0. If it does,
+        # then it means that there is an element that is not an integer.
+        # Then convert the temp list into an array.
+        if len(temp) > 0:
+            is_int = False
+            index = np.array(temp)
+        
+        # This conditional statement will return only a Boolean value if
+        # data is all integers. Otherwise, it will also return an array of 
+        # the indeces where there are non-integer values.
+        if is_int == True:
+            return is_int
+        else:
+            return [is_int, index]
